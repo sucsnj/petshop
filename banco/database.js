59b90f3e24a5601f8e93eed1ebf5f5ac8fbc6f38
Carlos <csnj@outlook.com>
2025-04-27 15:56:12 -0300

    alterações do banco, novas instruções

--- banco/database.js
+++ banco/database.js
@@ -2,6 +2,8 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./database.sqlite', (err) => {
     if (err) {
         console.error(err.message);
+        console.log('Erro ao conectar com o Banco de Dados');
+        return;
     }
     console.log('Conectado com o Banco de Dados');
 });
@@ -21,7 +23,7 @@ db.serialize(() => {
        `CREATE TABLE IF NOT EXISTS pets (
            id INTEGER PRIMARY KEY AUTOINCREMENT,
            name VARCHAR(150) NOT NULL,
-           species VARCHAR(150) NOT NULL UNIQUE,
+           species VARCHAR(150) NOT NULL,
            age INTEGER NOT NULL,
            tutorId INTEGER NOT NULL REFERENCES tutors(id)
        )`);
--- consultas.sql
+++ consultas.sql
@@ -1,2 +1,4 @@
-insert into tutors (name, email, phone) values ('exemplo', 'ex@doe.com', '123456789');
-select * from tutors;
+-- INSERT INTO tutors (name, email, phone) values ('exemplo', 'ex@doe.com', '123456789');
+-- DROP TABLE pets;
+-- SELECT * FROM tutors;
+SELECT * FROM pets;
