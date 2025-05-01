346c52df9bc5fc890a24ec6d3a59ccf8ccfb7f74
Carlos <csnj@outlook.com>
2025-04-30 22:07:07 -0300

    adição de partes do CRUD e mudança de view para ejs

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 
 app.use(cors());
 app.use(logger('dev'));
--- model/orders.js
+++ model/orders.js
@@ -113,7 +113,7 @@ function criarPedido(req, res) {
             return contagem;
         }, {});
 
-        // Inserir produtos no banco
+        // Inserir produtos no banco pedido_produtos
         Object.entries(qtdProduto).forEach(([productId, quantidade]) => {
             db.run(`INSERT INTO ${order_products}
                     (orderId, productId, quantidade)
@@ -149,10 +149,104 @@ function criarPedido(req, res) {
     });
 }
 
+function atualizarPedido(req, res) {
+    const id = Number(req.params.id);
+    const corpo = req.body;
+
+    // recupera o pedido com base no id
+    db.get(`SELECT * FROM ${banco} WHERE id= ?`, [id], (err, row) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Erro ao buscar o pedido' });
+        }
+        if (!row) {
+            return res.json('Pedido não encontrado.');
+        }
+
+        // copia todos os elementos do corpo da requisição para adentro do array novoCorpo
+        // caso não encontre no corpo, usa o que esta no pedido do banco de dados
+        const novoCorpo = {
+            tutorId: corpo.tutorId || row.tutorId,
+            petId: corpo.petId || row.petId,
+            total: corpo.total || row.total,
+            status: corpo.status || row.status,
+        };
+
+        // atualiza o pedido com base nas informações de novoCorpo (menos produtos e serviços)
+        db.run(`UPDATE ${banco} 
+            SET tutorId=?, petId=?, total=?, status=?
+            WHERE id=?`, [novoCorpo.tutorId, novoCorpo.petId, novoCorpo.total, novoCorpo.status, id], function (err) {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Erro ao atualizar o pedido' });
+            }
+
+            // apaga os antigos elementos da tabela que faz a comunicação entre produto e pedido, com base no id
+            db.run(`DELETE FROM ${order_products} WHERE orderId=?`, [id], (err) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).json({ error: 'Erro ao remover produtos antigos' });
+                }
+
+                let products = JSON.parse(corpo.products || '[]'); // converte os produtos para JSON e caso não tenha nada, retorna um array vazio []
+
+                // conta a quantidade de cada produto que foi passado pelo corpo da requisição
+                
+                // contagem é quem vai guardar a quantidade de cada produto, contagem é um objeto vazio inicialmente {}
+                // productId é o id do produto que foi passado pelo corpo da requisição
+                let qtdProduto = products.reduce((contagem, productId) => {
+                    contagem[productId] = (contagem[productId] || 0) + 1; // se contagem[productId], soma 1, e se for 0, insere o 0 e soma 1
+                    return contagem; // retorna um objeto com a contagem de cada productId, exemplo {1: 1, 2: 5, 3: 4, 4: 2}
+                }, {});
+
+                Object.entries(qtdProduto).forEach(([productId, quantidade]) => {
+                    db.run(`INSERT INTO ${order_products}
+                            (orderId, productId, quantidade)
+                            values
+                            (?, ?, ?)`, [id, productId, quantidade], function (err) {
+                        if (err) {
+                            console.error(err);
+                            return res.status(500).json({ error: 'Erro ao inserir produtos atualizados' });
+                        }
+                    });
+                });
+            });
+
+            // a mesma lógica de produtos será aplicada a serviços
+            db.run(`DELETE FROM ${order_services} WHERE orderId=?`, [id], (err) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).json({ error: 'Erro ao remover serviços antigos' });
+                }
+
+                let services = JSON.parse(corpo.services || '[]');
+                let qtdServico = services.reduce((contagem, serviceId) => {
+                    contagem[serviceId] = (contagem[serviceId] || 0) + 1;
+                    return contagem;
+                }, {});
+
+                Object.entries(qtdServico).forEach(([serviceId, quantidade]) => {
+                    db.run(`INSERT INTO ${order_services}
+                            (orderId, serviceId, quantidade)
+                            values
+                            (?, ?, ?)`, [id, serviceId, quantidade], function (err) {
+                        if (err) {
+                            console.error(err);
+                            return res.status(500).json({ error: 'Erro ao inserir serviços atualizados' });
+                        }
+                    });
+                });
+            });
+
+            return res.json('Pedido atualizado com sucesso!');
+        });
+    });
+}
+
 module.exports = {
     pegarPedidos,
     pegarPedidoPorId,
     criarPedido,
-    // atualizarPedido,
+    atualizarPedido,
     // apagarPedido
 };
