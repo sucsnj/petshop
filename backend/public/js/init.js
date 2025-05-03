eeef4f93557baf146c42e13b5fc9453b260d13ad
Carlos <csnj@outlook.com>
2025-05-03 11:21:29 -0300

    pequenas alterações

--- backend/public/js/init.js
+++
@@ -1,9 +0,0 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-    $('select').formSelect();
-    $('.tabs').tabs();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
